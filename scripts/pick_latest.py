f46141d108defff0c41658f5c713b4780f0cb02e
Felix Parker <flixpar@gmail.com>
2021-01-04 03:22:40 -0500

    fix pick_latest

--- scripts/pick_latest.py
+++ scripts/pick_latest.py
@@ -2,8 +2,12 @@ import shutil
 import glob
 
 fns = glob.glob("../public/results-static/*.json")
-maxdatestr = max([fn.split("/")[-1][:8] for fn in fns])
+dates = [fn.split("/")[-1][:8] for fn in fns]
+dates = [d for d in dates if d.isnumeric()]
+maxdatestr = max(dates)
 latestfns = [fn for fn in fns if maxdatestr in fn]
 
 for fn in latestfns:
+	print(fn, "=>", fn.replace(maxdatestr, "latest"))
 	shutil.copy(fn, fn.replace(maxdatestr, "latest"))
+
