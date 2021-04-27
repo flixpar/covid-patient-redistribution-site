1549366597978333ec495ed16f61a1b77d3c2dec
Felix Parker <flixpar@gmail.com>
2021-04-27 11:52:15 -0400

    precompute results script update

--- scripts/precompute_results.py
+++ scripts/precompute_results.py
@@ -1,33 +1,33 @@
 import requests
 
-URL = "https://covid-hospital-operations.com"
+URL = "https://ontario.covid-hospital-operations.com"
 
-regions = requests.get(f"{URL}/api/regions-list", params={"region_type": "state"}).json()
+regions = requests.get(f"{URL}/api/regions-list", params={"region_type": "province"}).json()
 regions = [r["region_id"] for r in regions]
 
-patient_types = ["icu", "acute", "all"]
+patient_types = ["icu"]
 covid_capacity_proportions = {"icu": "0.5", "acute": "0.3", "all": "0.4"}
 
 dates = requests.get(f"{URL}/json/dates.json").json()
-start_date = dates["forecast_start"]
-end_date = dates["forecast_end"]
+start_date = "2021-03-01"
+end_date = dates["data_end"]
 
 scenario = "moderate"
 
 default_params = {
-	"region_type": "state",
-	"hospitals": [],
+	"region_type": "province",
+	"locations": [],
 	"scenario": scenario,
 	"objective": "minoverflow",
 	"integer": "false",
 	"transferbudget": "1000",
 	"utilization": "1.0",
-	"dist_threshold": "600",
+	"dist_threshold": "1000",
 	"uncertaintylevel": "default",
-	"los": "regional_dist",
+	"los": "default_dist",
 	"start_date": start_date,
 	"end_date": end_date,
-	"max_hospitals": 150,
+	"max_locations": 150,
 	"smoothness": "false",
 	"verbose": "false",
 }
@@ -38,21 +38,22 @@ for patient_type in patient_types:
 	for region_id in regions:
 		print("Region:", region_id)
 
-		hospitals = requests.get(f"{URL}/api/hospital-list", params={"region_type": default_params["region_type"], "region_id": region_id}).json()
-		if len(hospitals) > default_params["max_hospitals"]:
-			hospitals = sorted(hospitals, key=(lambda h: h["total_beds"]), reverse=True)
-			hospital_ids = [h["hospital_id"] for h in hospitals[:default_params["max_hospitals"]]]
-			hospitals_list = {"hospitals": hospital_ids}
+		locations = requests.get(f"{URL}/api/locations-list", params={"region_type": default_params["region_type"], "region_id": region_id}).json()
+		if len(locations) > default_params["max_locations"]:
+			locations = sorted(locations, key=(lambda h: h["beds"]), reverse=True)
+			locations_ids = [h["location_id"] for h in locations[:default_params["max_locations"]]]
+			locations_list = {"locations": locations_ids}
 		else:
-			hospitals_list = {}
+			locations_list = {}
 
-		params = {**default_params, **hospitals_list,
+		params = {**default_params, **locations_list,
 			"region_id": region_id,
 			"patient_type": patient_type,
 			"covid_capacity_proportion": covid_capacity_proportion,
 		}
 
-		fn = f"../public/results-static/latest_{scenario}_{patient_type}_state_{region_id}.json"
+		region_type = default_params["region_type"]
+		fn = f"../public/results-static/latest_{scenario}_{patient_type}_{region_type}_{region_id}.json"
 
 		response = requests.post(f"{URL}/api/patients", json=params)
 		with open(fn, "w") as f:
