fc6ddecbff500961bb3008d4ab1b5f7594b0dfcc
Felix Parker <flixpar@gmail.com>
2023-01-10 10:21:43 -0500

    removed manual corrections of hhs data

--- process_data/convert_hhs.jl
+++ process_data/convert_hhs.jl
@@ -19,10 +19,6 @@ function convert_hhs_data(;icu_pct=0.3)
 	end
 	filter!(row -> !(row.hospital_pk in bad_ids), rawdata)
 
-	# manual corrections
-	rawdata[(rawdata.hospital_pk .== "140064") .& (rawdata.collection_week .> "2021/10/01"), :staffed_icu_adult_patients_confirmed_covid_7_day_sum] .= missing
-	rawdata[(rawdata.hospital_pk .== "140113") .& (rawdata.collection_week .> "2021/10/01"), :staffed_icu_adult_patients_confirmed_covid_7_day_sum] .= missing
-
 	data_weekly = select(rawdata,
 		:hospital_name => :hospital,
 		:hospital_pk => :hospital_id,
