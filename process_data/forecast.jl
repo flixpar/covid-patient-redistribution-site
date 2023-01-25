a21bb2830dc6fbc94a6696beadfd79d0abe3430d
Felix Parker <flixpar@gmail.com>
2023-01-25 12:50:55 -0500

    overhaul forecast disaggregation

--- process_data/forecast.jl
+++ process_data/forecast.jl
@@ -1,245 +1,257 @@
 using CSV
-using Dates
-using Glob
-using Base.Threads
-using ProgressMeter
 using DataFrames
+using Glob, Dates
 
-include("util.jl")
 
+function main()
+	forecast_date = latest_forecast_date()
+	forecast_cols = [:admissions_combined, :active_combined]
+	history_weeks = 12
 
-function disaggregate_forecast(forecast_date="latest"; uncertainty_version=:default, write_versioned=false, VERBOSE=false, DEBUG=false)
+	forecast = compute_forecast(forecast_date, forecast_cols, history_weeks)
+	forecast |> CSV.write("../data/forecasts/forecast-$(forecast_date).csv")
 
-	forecast_cols = [:admissions_icu, :admissions_acute, :admissions_combined_ped]
+	return
+end
 
-	hosp_data_all = DataFrame(CSV.File("../data/hospitalization_data.csv"))
-	for col in forecast_cols
-		hosp_data_all[:,col] = coalesce.(hosp_data_all[:,col], 0)
-	end
+function disaggregate_forecast(forecast_date="latest"; uncertainty_type=:quantile, write_versioned=false)
+	@assert uncertainty_version == :none
 
-	hosp_metadata = DataFrame(CSV.File("../data/hhs_hospital_meta.csv"))
+	forecast_date = (forecast_date == "latest") ? latest_forecast_date() : forecast_date
+	forecast_cols = [:admissions_combined, :admissions_icu, :admissions_acute, :admissions_combined_ped, :active_combined]
+	history_weeks = 12
 
-	cases_data_all = DataFrame(CSV.File("../rawdata/csse_confirmed_cases.csv"))
-
-	if forecast_date == "latest"
-		forecast_date = latest_forecast_date()
-	end
-
-	fn_ext = (forecast_date >= Date(2021, 9, 27)) ? "COVIDhub-4_week_ensemble" : "COVIDhub-ensemble"
-	fn = "../rawdata/forecasts/$(forecast_date)-$(fn_ext).csv"
-	forecast = DataFrame(CSV.File(fn))
-
-	filter!(row -> contains(row.target, "inc case"), forecast)
-	filter!(row -> row.location != "US", forecast)
-	forecast.location = parse.(Int, forecast.location)
-
-	forecast.weeks_out = map(s -> parse(Int, s[1]), forecast.target)
-	select!(forecast, Not(:target))
-
-	if uncertainty_version == :default
-
-		uncertainty_scale = [1.0, 1.0, 1.0, 1.0]
-
-		pct_lb, pct_ub = 0.25, 0.75
-		forecast.quantile = map(q -> (q == "NA") ? :point : parse(Float64, q), forecast.quantile)
-		filter!(row -> row.quantile == :point || row.quantile == pct_lb || row.quantile == pct_ub, forecast)
-		forecast.bound = map(q -> (q == :point) ? :est : (q == pct_lb) ? :lb : :ub, forecast.quantile)
-		select!(forecast, Not([:quantile, :type]))
-		forecast = unstack(forecast, :bound, :value)
-
-		forecast.lb = map(r -> r.est + ((r.lb - r.est) * uncertainty_scale[r.weeks_out]), eachrow(forecast))
-		forecast.ub = map(r -> r.est + ((r.ub - r.est) * uncertainty_scale[r.weeks_out]), eachrow(forecast))
-
-	elseif uncertainty_version == :alternate
-
-		quantiles = [(0.25, 0.75), (0.25, 0.75), (0.1, 0.9), (0.025, 0.975)]
-		forecast.quantile = map(q -> (q == "NA") ? :point : parse(Float64, q), forecast.quantile)
-		forecast.bound = map(eachrow(forecast)) do r
-			if r.quantile == :point
-				:est
-			elseif r.quantile == quantiles[r.weeks_out][1]
-				:lb
-			elseif r.quantile == quantiles[r.weeks_out][2]
-				:ub
-			else
-				:na
-			end
-		end
-		filter!(r -> r.bound != :na, forecast)
-		select!(forecast, Not([:quantile, :type, :weeks_out]))
-		forecast = unstack(forecast, :bound, :value)
-
-	else
-
-		filter!(row -> row.type == "point", forecast)
-		forecast.est = forecast.value
-		forecast.lb = forecast.value
-		forecast.ub = forecast.value
-
-	end
-
-	hosp_data = filter(row -> row.date <= forecast_date, hosp_data_all)
-	cases_data = filter(row -> row.date <= forecast_date, cases_data_all)
-
-	recent_dates = sort(intersect(unique(hosp_data.date), unique(cases_data.date)))[end-14+1:end]
-
-	total_hosp_recent = filter(row -> row.date in recent_dates, hosp_data)
-	total_hosp_recent = combine(groupby(total_hosp_recent, :hospital_id),
-		[col => sum => col for col in forecast_cols]...,
-	)
-	total_hosp_recent_dict = Dict(row.hospital_id => row for row in eachrow(total_hosp_recent))
-
-	total_cases_recent = filter(row -> row.date in recent_dates, cases_data)
-	total_cases_recent = combine(groupby(total_cases_recent, :county_fips), :cases => sum => :cases)
-	total_cases_recent_dict = Dict(row.county_fips => row.cases for row in eachrow(total_cases_recent))
-
-	if VERBOSE
-		@show forecast_date
-		@show recent_dates
-	end
-
-	h_forecasts = []
-
-	results_lock = ReentrantLock()
-	io_lock = ReentrantLock()
-
-	counties = sort(unique(forecast.location))
-	if DEBUG counties = counties[1:60] end
-
-	p = Progress(length(counties))
-
-	@threads for county in counties
-		county_hosp_ind = findall(f -> !ismissing(f) && f == county, hosp_metadata.county_fips)
-
-		if isempty(county_hosp_ind)
-			next!(p)
-			continue
-		end
-
-		county_hosp_ids = unique(hosp_metadata[county_hosp_ind, :hospital_id])
-
-		total_cases = max(1, total_cases_recent_dict[county])
-
-		hist = Dict(col => [haskey(total_hosp_recent_dict,h) ? total_hosp_recent_dict[h][col] : 0 for h in county_hosp_ids] for col in forecast_cols)
-		hist_total = Dict(col => sum(hist[col]) for col in forecast_cols)
-		scale = Dict(col => hist_total[col] / total_cases for col in forecast_cols)
-
-		hosp_weights = Dict(col => hist[col] .* (scale[col] / hist_total[col]) for col in forecast_cols)
-		for col in forecast_cols
-			z = sum(hosp_weights[col])
-			map!(x -> (z==0.0 || isnan(x)) ? 0.0 : x/z, hosp_weights[col], hosp_weights[col])
-		end
-
-		county_forecast = filter(row -> row.location == county, forecast)
-
-		day0 = minimum(county_forecast.target_end_date) - Day(7)
-		last_cases_df = filter(row -> row.county_fips == county && (day0-Day(7) < row.date <= day0), cases_data)
-		last_cases = sum(last_cases_df.cases)
-
-		dfs = DataFrame[]
-		for (i,hid) in enumerate(county_hosp_ids)
-			w = Dict(col => hosp_weights[col][i] for col in forecast_cols)
-
-			h_forecast = select(county_forecast,
-				:target_end_date => :date,
-				:weeks_out,
-				[:est => ByRow(x -> x * w[col]) => col for col in forecast_cols]...,
-				[:lb => ByRow(x -> x * w[col]) => "$(col)_lb" for col in forecast_cols]...,
-				[:ub => ByRow(x -> x * w[col]) => "$(col)_ub" for col in forecast_cols]...,
-			)
-			insertcols!(h_forecast, 1, :hospital_id => fill(hid, nrow(h_forecast)))
-
-			push!(h_forecast, Dict(
-				"hospital_id" => hid,
-				"date" => day0,
-				"weeks_out" => 0,
-				["$(col)" => last_cases * w[col] for col in forecast_cols]...,
-				["$(col)_lb" => last_cases * w[col] for col in forecast_cols]...,
-				["$(col)_ub" => last_cases * w[col] for col in forecast_cols]...,
-			))
-
-			push!(dfs, h_forecast)
-		end
-
-		c_forecast = vcat(dfs...)
-		lock(results_lock) do
-			push!(h_forecasts, c_forecast)
-		end
-
-		if VERBOSE
-			lock(io_lock) do
-				@show county
-				@show county_hosp_ids
-				@show total_admissions_icu
-				@show total_admissions_acute
-				@show total_cases
-				@show scale_icu, scale_acute
-				@show hosp_weights_icu
-				@show hosp_weights_acute
-				@show county_forecast
-				for df in dfs
-					@show df
-				end
-				print("\n\n=================================\n\n")
-				flush(stdout)
-			end
-		end
-
-		next!(p)
-	end
-
-	hospitalization_forecast = vcat(h_forecasts...)
-	sort!(hospitalization_forecast, [:hospital_id, :date])
-
-	hospitalization_forecast.admissions_combined = hospitalization_forecast.admissions_icu + hospitalization_forecast.admissions_acute
-	hospitalization_forecast.admissions_combined_lb = hospitalization_forecast.admissions_icu_lb + hospitalization_forecast.admissions_acute_lb
-	hospitalization_forecast.admissions_combined_ub = hospitalization_forecast.admissions_icu_ub + hospitalization_forecast.admissions_acute_ub
+	forecast = compute_forecast(forecast_date, forecast_cols, history_weeks)
 
 	if write_versioned
-		hospitalization_forecast |> CSV.write("../data/forecasts/forecast-$(forecast_date).csv")
+		forecast |> CSV.write("../data/forecasts/forecast-$(forecast_date).csv")
 	else
-		hospitalization_forecast |> CSV.write("../data/hospitalization_forecast.csv")
+		forecast |> CSV.write("../data/hospitalization_forecast.csv")
 	end
 
 	return
 end
 
-function convert_cases()
-	rawdata = DataFrame(CSV.File("../rawdata/time_series_covid19_confirmed_US.csv"))
+function compute_forecast(forecast_date, forecast_cols, history_weeks, uncertainty_type)
 
-	date_cols = filter(x -> contains(x, "/"), names(rawdata))
-	data = stack(rawdata, date_cols, variable_name=:date_str, value_name=:cum_cases)
-	data.date = map(d -> Date(d, "m/d/yy")+Year(2000), data.date_str)
-	dropmissing!(data, [:FIPS, :date])
+	# load hospitalizations data
+	hosp_data, hosp_data_mean = load_hospitalization_data(forecast_date, forecast_cols, history_weeks)
+	hospital_ids = sort(unique(hosp_data.hospital_id))
+	hist_start_date, hist_end_date = extrema(hosp_data.date)
 
-	select!(data,
-		:UID => :county_uid,
-		:FIPS => ByRow(x -> Int(x)) => :county_fips,
-		:Admin2 => :county_name,
-		:Province_State => :state,
+	# load hospital metadata
+	hosp_metadata = load_hospital_metadata(hospital_ids)
+	counties_hosp = unique(hosp_metadata.county_fips)
+
+	# load cases data
+	cases_data, cases_data_mean = load_cases_data(hist_start_date, hist_end_date)
+	counties_cases = unique(cases_data.fips)
+	counties = sort(intersect(counties_hosp, counties_cases))
+
+	# load cases forecast
+	cases_forecast, quantiles, forecast_target_dates = load_cases_forecast(forecast_date, counties, uncertainty_type)
+	max_weeks_out = size(cases_forecast, 2)
+
+	# construct mapping from counties to hospitals
+	county_to_hosp = Dict(c => String[] for c in counties)
+	for row in eachrow(hosp_metadata)
+		if haskey(county_to_hosp, row.county_fips)
+			push!(county_to_hosp[row.county_fips], row.hospital_id)
+		end
+	end
+
+	# compute the aggregation weights
+	weights = zeros(length(forecast_cols), length(hospital_ids), length(counties))
+	hospital_inds = Dict(hospital_ids .=> 1:length(hospital_ids))
+	for (k, col) in enumerate(forecast_cols)
+		for (j, county) in enumerate(counties)
+			county_hosp_ids = county_to_hosp[county]
+
+			county_hosps = [haskey(hosp_data_mean, h) ? hosp_data_mean[h][col] : 0 for h in county_hosp_ids]
+			county_weights = county_hosps ./ cases_data_mean[county]
+
+			for (i, h) in enumerate(county_hosp_ids)
+				if haskey(hospital_inds, h)
+					weights[k, hospital_inds[h], j] = county_weights[i]
+				end
+			end
+		end
+	end
+	replace!(weights, NaN => 0, Inf => 0)
+
+	# compute the forecast
+	hosp_forecast = Array{Float64, 4}(undef, length(forecast_cols), length(hospital_ids), max_weeks_out, length(quantiles))
+	for (k, col) in enumerate(forecast_cols)
+		for q in 1:length(quantiles)
+			@views hosp_forecast[k, :, :, q] = weights[k, :, :] * cases_forecast[:, :, q]
+		end
+	end
+
+	# transform forecast into dataframe
+	M = length(forecast_cols)
+	N = length(hospital_ids)
+	T = max_weeks_out
+	Q = length(quantiles)
+
+	hosp_forecast_df = DataFrame(
+		hospital_id = [hospital_ids[i] for m in 1:M, i in 1:N, j in 1:T, q in 1:Q] |> vec,
+		weeks_out = [j for m in 1:M, i in 1:N, j in 1:T, q in 1:Q] |> vec,
+		metric = [forecast_cols[m] for m in 1:M, i in 1:N, j in 1:T, q in 1:Q] |> vec,
+		quantile = [quantiles[q] for m in 1:M, i in 1:N, j in 1:T, q in 1:Q] |> vec,
+		value = [hosp_forecast[m, i, j, q] for m in 1:M, i in 1:N, j in 1:T, q in 1:Q] |> vec,
+	)
+
+	hosp_forecast_df = unstack(hosp_forecast_df, :metric, :value)
+	insertcols!(hosp_forecast_df, 2, :forecast_date => forecast_date)
+	insertcols!(hosp_forecast_df, 3, :target_date => map(w -> forecast_target_dates[w], hosp_forecast_df.weeks_out))
+
+	sort!(hosp_forecast_df, [:hospital_id, :weeks_out, :quantile])
+
+	if uncertainty_type == :none
+		select!(hosp_forecast_df, Not(:quantile))
+	end
+
+	return hosp_forecast_df
+end
+
+function load_hospitalization_data(forecast_date, forecast_cols, history_weeks)
+	hosp_data = DataFrame(CSV.File("../data/hospitalization_data.csv", select=[:hospital_id, :date, forecast_cols...]))
+
+	hosp_dates = sort(unique(hosp_data.date))
+	forecast_date_idx = searchsortedfirst(hosp_dates, forecast_date) - 1
+	hist_start_date = hosp_dates[forecast_date_idx - history_weeks]
+	hist_end_date = hosp_dates[forecast_date_idx]
+
+	filter!(r -> hist_start_date <= r.date <= hist_end_date, hosp_data)
+	select!(hosp_data, :hospital_id, :date, forecast_cols...)
+	sort!(hosp_data, [:hospital_id, :date])
+
+	hosp_data_mean = combine(
+		groupby(hosp_data, :hospital_id),
+		forecast_cols .=> (mean ∘ nonmissing) .=> forecast_cols,
+	)
+	hosp_data_mean = Dict(r.hospital_id => r for r in eachrow(hosp_data_mean))
+
+	return hosp_data, hosp_data_mean
+end
+
+function load_cases_data(start_date, end_date)
+	cases_rawdata = DataFrame(CSV.File("../rawdata/time_series_covid19_confirmed_US.csv"))
+	cases_data = process_cases(cases_rawdata)
+	filter!(r -> start_date <= r.date <= end_date, cases_data)
+
+	cases_data_mean = combine(
+		groupby(cases_data, :fips),
+		:cases => (mean ∘ nonmissing) => :cases,
+	)
+	cases_data_mean = Dict(r.fips => max(r.cases * 7, 0) for r in eachrow(cases_data_mean))
+
+	return cases_data, cases_data_mean
+end
+
+function load_cases_forecast(forecast_date, counties, uncertainty_type)
+	cases_forecast_raw = DataFrame(CSV.File(forecast_fn(forecast_date)))
+	cases_forecast = process_hub_forecast(cases_forecast_raw, uncertainty_type)
+
+	max_weeks_out = maximum(cases_forecast.weeks_out)
+	forecast_target_dates = sort(unique(cases_forecast.target_date))
+	quantiles = sort(unique(cases_forecast.quantile))
+
+	cases_forecast_array = zeros(Float64, length(counties), max_weeks_out, length(quantiles))
+	county_inds = Dict(counties .=> 1:length(counties))
+	quantile_inds = Dict(quantiles .=> 1:length(quantiles))
+	for row in eachrow(cases_forecast)
+		if haskey(county_inds, row.fips)
+			cases_forecast_array[county_inds[row.fips], row.weeks_out, quantile_inds[row.quantile]] = row.cases
+		end
+	end
+
+	return cases_forecast_array, quantiles, forecast_target_dates
+end
+
+function load_hospital_metadata(hospital_ids)
+	hosp_metadata = DataFrame(CSV.File("../data/hhs_hospital_meta.csv"))
+	dropmissing!(hosp_metadata, [:hospital_id, :county_fips])
+	filter!(r -> r.hospital_id in hospital_ids, hosp_metadata)
+	return hosp_metadata
+end
+
+function latest_forecast_date()
+	paths = glob("../rawdata/forecasts/*-COVIDhub-4_week_ensemble.csv")
+	if isempty(paths) return nothing end
+	date_strs = [basename(p)[1:10] for p in paths]
+	dates = [Date(d) for d in date_strs]
+	date = maximum(dates)
+	return date
+end
+
+function forecast_fn(forecast_date)
+	fn_ext = (forecast_date >= Date(2021, 9, 27)) ? "COVIDhub-4_week_ensemble" : "COVIDhub-ensemble"
+	fn = "../rawdata/forecasts/$(forecast_date)-$(fn_ext).csv"
+	return fn
+end
+
+function process_cases(cases_rawdata)
+	date_cols = filter(x -> contains(x, "/"), names(cases_rawdata))
+	cases_data = stack(cases_rawdata, date_cols, variable_name=:date_str, value_name=:cum_cases)
+	cases_data.date = map(d -> Date(d, dateformat"m/d/yy")+Year(2000), cases_data.date_str)
+	dropmissing!(cases_data, [:FIPS, :date])
+
+	select!(cases_data,
+		:FIPS => ByRow(x -> Int(x)) => :fips,
 		:date,
 		:cum_cases,
 	)
-	sort!(data, [:county_fips, :date])
 
-	new_cases = combine(groupby(data, :county_fips),
+	new_cases = combine(groupby(cases_data, :fips),
 		:date,
 		:cum_cases => (xs -> vcat(xs[1], diff(xs))) => :cases,
 	)
+	cases_data = leftjoin(cases_data, new_cases, on=[:fips, :date])
 
-	data = leftjoin(data, new_cases, on=[:county_fips, :date])
-
-	data |> CSV.write("../rawdata/csse_confirmed_cases.csv")
-
-	return
+	return cases_data
 end
 
-function disaggregate_forecast_all()
-	fns = glob("../rawdata/forecasts/*.csv")
-	dates = [Date(basename(fn)[1:10]) for fn in fns]
-	sort!(dates, rev=true)
-	for d in dates
-		disaggregate_forecast(d, write_versioned=true)
+function process_hub_forecast(forecast_raw, uncertainty_type=:quantile)
+	forecast = filter(row -> contains(row.target, "inc case"), forecast_raw)
+	rename!(forecast, :value => :cases)
+
+	filter!(row -> row.location != "US", forecast)
+	rename!(forecast, :location => :fips)
+	forecast.fips = map(x -> parse(Int, x), forecast.fips)
+
+	weeks_out = map(s -> parse(Int, s[1]), forecast.target)
+	insertcols!(forecast, 4, :weeks_out => weeks_out)
+	select!(forecast, Not(:target))
+
+	if uncertainty_type == :quantile
+		filter!(row -> row.type == "quantile", forecast)
+		select!(forecast, Not(:type))
+		forecast.quantile = map(x -> parse(Float64, x), forecast.quantile)
+	elseif uncertainty_type == :none
+		filter!(row -> row.type == "point", forecast)
+		select!(forecast, Not(:quantile))
+		rename!(forecast, :type => :quantile)
+	else
+		error("Unknown uncertainty type: $uncertainty_type")
 	end
-	return
+
+	rename!(forecast, :target_end_date => :target_date)
+
+	sort!(forecast, [:fips, :weeks_out])
+	return forecast
+end
+
+function mean(xs; init=0)
+	if isempty(xs)
+		return init
+	else
+		return sum(xs) / length(xs)
+	end
+end
+
+nonmissing(xs) = filter(!ismissing, xs)
+
+if abspath(PROGRAM_FILE) == @__FILE__
+	main()
 end
